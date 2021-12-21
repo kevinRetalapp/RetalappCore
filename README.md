828c49d298cc55b918110ddbd03fb610ebe16695
kevinRetalapp <kevin.steven@retalapp.co>
2021-12-21 10:49:45 -0500

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="#">
+    <img src="https://res.cloudinary.com/kentruri/image/upload/v1640101557/Logo_White_BG_mxbutd.png" alt="Retalapp logo" width="200" height="165">
+  </a>
+</p>
+
+<p align="center">
+  CSS Component library
+</p>
+
+
+
+## Quick start
+
+Several quick start options are available:
+
+- Clone the repo: `git clone https://github.com/`
+- Install with [npm](https://www.npmjs.com/): `npm install @retalapp/core`
+- With CDN 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,8 +3,8 @@ const config = require('./config.templates')
 const {cssConfig,optimizeCss,resolve,production,pluginsCss} = config
 module.exports = [
   {
-    entry: {core: "./packages/core/lib/index.js",},
-    output: {path: path.resolve(__dirname, "./packages/core/build/"), filename: "[name].js",},
+    entry: {core: "/packages/core/lib/index.js",},
+    output: {path: path.resolve(__dirname, "../packages/core/build/"), filename: "[name].js",},
     optimization: optimizeCss ,
     plugins: pluginsCss,
     module: cssConfig,
@@ -12,8 +12,8 @@ module.exports = [
     resolve,
   },
   {
-    entry: {component: "./packages/component/lib/index.js",},   
-    output: {path: path.resolve(__dirname, "./packages/component/build/"),filename: "[name].js",},
+    entry: {component: "/packages/component/lib/index.js",},   
+    output: {path: path.resolve(__dirname, "../packages/component/build/"),filename: "[name].js",},
     module: {
         rules: [
           {
